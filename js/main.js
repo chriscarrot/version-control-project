d43241d8d879f433671d3a6caf15a3ffc22758c1
Micah Fischer <mfischer981@west-mec.org>
2018-10-01 06:39:49 -0700

    Fixed Links on Mobile
    Fixed links that wouldn't hide on mobile.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 $(window).scroll(function() {
     if ($(this).scrollTop() > 100) { //use `this`, not `document`
         $('nav h1').css({
@@ -16,8 +35,3 @@ $(window).scroll(function() {
 });
 
 
-// Dropdown Menu
-var urlmenu = document.getElementById( 'menu' );
-urlmenu.onchange = function() {
-     window.open( this.options[ this.selectedIndex ].value );
-};
